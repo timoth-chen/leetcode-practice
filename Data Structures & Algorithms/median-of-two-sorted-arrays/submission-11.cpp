a64868c945f1e5f13de587b704379e0cee4e52f4
Timothy Chen <45443533+timoth-chen@users.noreply.github.com>
2026-09-21 11:02:40 -0400

    Add: median-of-two-sorted-arrays - submission-11

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-11.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>& A = nums1.size() <= nums2.size() ? nums1 : nums2;
+        vector<int>& B = nums1.size() <= nums2.size() ? nums2 : nums1;
+        int m = A.size(), n = B.size();
+        int total = m + n;
+        int half = (total + 1)/2;
+
+        int l = 0, r = m;
+        while (l <= r){
+            int i = l + (r - l)/2;
+            int j = half - i;
+
+            int leftA = i > 0 ? A[i - 1] : INT_MIN;
+            int rightA = i < m ? A[i] : INT_MAX;
+            int leftB = j > 0 ? B[j - 1] : INT_MIN;
+            int rightB = j < n ? B[j] : INT_MAX;
+            
+            if (leftA <= rightB && leftB <= rightA){
+                if (total % 2 != 0){
+                    return max(leftA, leftB);
+                }
+                return (max(leftA, leftB) + min(rightA, rightB))/2.0;
+            } else if(leftA > rightB){
+                r = i - 1;
+            } else{
+                l = i + 1;
+            }
+        }
+        return - 1;
+    }
+};
